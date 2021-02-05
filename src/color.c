fea06749c56fbc6ec5c57296f5ca31848d00430c
Richard Chennault <rchennau@users.noreply.github.com>
2021-02-04 17:16:13 -0800

    Added color cycle

--- src/color.c
+++ src/color.c
@@ -14,7 +14,9 @@ extern const char text[]; // Defined in text.s file
 #define SAVMSC *(unsigned int *) 88 // default screen address for the Atari
 
 void __fastcall__ str_to_internal(char *s);  // convert string in memory to internal character format Atasci
+
 void graphics2();
+
 unsigned char * display_list();
 void custom_screen(char *);
 
@@ -22,20 +24,84 @@ int main (void) {
 	unsigned int y;	
 	// static int *x;
 	unsigned char *dlist;
+	unsigned char *color;
+	unsigned char *hue;
+	unsigned char *luminance;
+	unsigned char *keypress;
+	unsigned char *keycode;
 	// x = display_list();
 	dlist = display_list();
-	POKE(dlist+3,71);
-	POKE(dlist+6,7);
-	POKE(709,12);
-	POKE(710,110);
-	POKE(712,110);
+	// POKE(dlist+3,71);
+	// POKE(dlist+6,7);
+	// POKE(710,207);
+	// POKE(712,207);
+	// POKE(711,207);
+	_graphics(0);
 	gotoxy(0,0);
-	printf("Starting up!\n");
-	sleep(4);
+	color = PEEK(710);
+	printf("Press up/down to change Hue\nPress left/right to change luminance\n");
+	printf("The current color: %i, %i, %i\n", PEEK(710), PEEK(711), PEEK(712));
+
+	/*******************************************************************
+	 * Test for up, down, left and right and set the color accordingly *
+	 * 
+	 */
+	
+	while (1) {
+		*keypress = cgetc();
+		gotoxy(0,14);
+		if ((*keypress) == 28) {
+			printf("Key pressed up : %i\t", *keypress);
+			// color = *hue*16+1+*luminance;
+			hue = *hue*16+1;
+			printf("Peek = %i",PEEK(710));
+		} 
+		if ((*keypress) == 29) {
+			printf("Key pressed down : %i\t", *keypress);
+			// color = color - *hue*16-1+*luminance;
+			hue = *hue*16-1;
+			printf("Peek = %i",PEEK(710));
+		} 
+		if ((*keypress) == 30) {
+			printf("Key pressed left : %i\t", *keypress);
+			// color = color + *hue*16+*luminance+1;
+			luminance++;
+			printf("Peek = %i",PEEK(710));
+		} 
+		if ((*keypress) == 31) {
+			printf("Key pressed right : %i\t", *keypress);
+			luminance--;
+			printf("Peek = %i",PEEK(710));
+		} 
+		color = *hue*16+*luminance;
+		POKE(710,color);
+		printf("Peek = %i",PEEK(710));
+	}
+	keypress = cgetc();
+	printf("Keypressed : %i\n", *keycode);
+	keycode = (*keypress/64)-(*keycode*64);
+	printf("Keypressed : %i\n", *keycode);
+	// if (keypress = )	
+	printf("Set Hue:");
+	cscanf("%i",hue);
+	gotoxy(0,20);
+	printf("\tHue set: %i", *hue);
+	printf("\nSet Luminance:");
+	cscanf("%i",luminance);
+	printf("\tLuminance set:%i\n", *luminance);
+	color = *hue*16+luminance;
+	POKE(710,color);
+	// POKE(711,hue);
+	// POKE(712,luminance);
+	
+	printf("What is the current color: %i, %i, %i\n", PEEK(710), PEEK(711), PEEK(712));
+	sleep(4); 
+	/*
 	for (y = 0; y <= 31; y++) {
 	 	printf("Display List %i\t", dlist[y]);	
 	}
-	while(5);
+	*/
+	while(1);
 	return EXIT_SUCCESS;
 }
 /*********************************************************************
